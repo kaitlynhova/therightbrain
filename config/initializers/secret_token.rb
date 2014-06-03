e2de4bd9ed057b7ab30e791dcd48479b77e9e09d
kaitlynhova <kaitlynhova@gmail.com>
2014-06-02 22:32:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRightBrain::Application.config.secret_token = '9268543acc8644b23a7703f22677d1b1d6402b93ba92b170cf29c6c31dfcd7d04373c968474da4fa922ce181c17b6eddabe43271c09e2fa5537902e0805d604c'
