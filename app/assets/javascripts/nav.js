b561f3fcd0fc669eab511675f1d576a5ec56854b
kaitlynhova <kaitlynhova@gmail.com>
2014-06-05 16:50:42 -0500

    start to menu

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
       $('#that_triangle').css('top', '68px');
     }
   });
+  $( "#media" ).click(function() {
+    $('#that_triangle').slideToggle('triangle_submenu');
+    $('#media_submenu').slideToggle('goaway');
+    if ($('#that_triangle').position().top == 68){
+      $('#that_triangle').css('top', '108px');
+    }else if ($('#that_triangle').position().top == 108){
+      $('#that_triangle').css('top', '68px');
+    }
+  });
+  
 });
