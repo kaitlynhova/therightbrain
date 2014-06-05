0c7b6bf80a99f747819f125f23b0241685635078
kaitlynhova <kaitlynhova@gmail.com>
2014-06-04 23:20:35 -0500

    this again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,6 @@ TheRightBrain::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  config.assets.precompile += %w(masonry.js grid3d.js)
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
