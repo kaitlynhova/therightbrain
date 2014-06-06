86bc20ebeafa8712ec5c91189ec30bda5d2c8624
kaitlynhova <kaitlynhova@gmail.com>
2014-06-06 17:51:33 -0500

    no more precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ TheRightBrain::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.assets.precompile += %w(masonry.js component.css fancybox.js fancyboxmedia.js)
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
