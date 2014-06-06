2c764f4801ca0d982754bad0a80a32cc91e6befd
kaitlynhova <kaitlynhova@gmail.com>
2014-06-06 17:49:05 -0500

    commented out top of fb media code

--- app/assets/javascripts/fancyboxmedia.js
+++ app/assets/javascripts/fancyboxmedia.js
@@ -1,4 +1,4 @@
-
+/*!
  * Media helper for fancyBox
  * version: 1.0.6 (Fri, 14 Jun 2013)
  * @requires fancyBox v2.0 or later
@@ -62,7 +62,7 @@
  *          http://maps.google.com/?ll=48.857995,2.294297&spn=0.007666,0.021136&t=m&z=16
  *          http://maps.google.com/?ll=48.859463,2.292626&spn=0.000965,0.002642&t=m&z=19&layer=c&cbll=48.859524,2.292532&panoid=YJ0lq28OOy3VT2IqIuVY0g&cbp=12,151.58,,0,-15.56
  */
-;(function ($) {
+(function ($) {
 	"use strict";
 
 	//Shortcut for fancyBox object
