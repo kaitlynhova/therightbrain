8a353717a526ece0eeda2200323adc2541e066f1
kaitlynhova <kaitlynhova@gmail.com>
2014-06-06 17:28:14 -0500

    logging line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ TheRightBrain::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  
+  config.logger = Logger.new(STDOUT)
 
   # Generate digests for assets URLs
   config.assets.digest = true
