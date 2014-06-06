2296bd354a8dae86ebc5671b71436696d91c5aac
kaitlynhova <kaitlynhova@gmail.com>
2014-06-06 15:49:37 -0500

    mobile bar nav

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,41 +1,21 @@
 $(document).ready(function(){
   $( "#music" ).click(function() {
-    //$('#music_submenu').removeClass('goaway');
-    $('#music_submenu').css('opacity', '1.0');
-    $('#music_submenu').css('top', '0px');
-    // $('#that_triangle').slideToggle('triangle_submenu');
-    // $('#music_submenu').slideToggle('goaway');
-    // if ($('#that_triangle').position().top == 68){
-    //   $('#that_triangle').css('display', 'none');
-    // }else if ($('#that_triangle').position().top == 108){
-    //   $('#that_triangle').css('top', '68px');
-    // }
+    $('#that_triangle').slideToggle('triangle_submenu');
+    $('#music_submenu').slideToggle('goaway');
+    if ($('#that_triangle').position().top == 68){
+      $('#that_triangle').css('display', 'none');
+    }else if ($('#that_triangle').position().top == 108){
+      $('#that_triangle').css('top', '68px');
+    }
   });
   $( "#media" ).click(function() {
-    // $('#that_triangle').slideToggle('triangle_submenu');
-    // $('#media_submenu').slideToggle('goaway');
-    // if ($('#that_triangle').position().top == 68){
-    //   $('#that_triangle').css('top', '108px');
-    // }else if ($('#that_triangle').position().top == 108){
-    //   $('#that_triangle').css('top', '68px');
-    // }
-  });
-  
-  var anchor = document.querySelectorAll('button');
-  
-  [].forEach.call(anchor, function(anchor){
-    var open = false;
-    anchor.onclick = function(event){
-      event.preventDefault();
-      if(!open){
-        this.classList.add('close');
-        open = true;
-      }
-      else{
-        this.classList.remove('close');
-        open = false;
-      }
+    $('#that_triangle').slideToggle('triangle_submenu');
+    $('#media_submenu').slideToggle('goaway');
+    if ($('#that_triangle').position().top == 68){
+      $('#that_triangle').css('top', '108px');
+    }else if ($('#that_triangle').position().top == 108){
+      $('#that_triangle').css('top', '68px');
     }
-  }); 
+  });
   
 });
