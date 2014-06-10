bbead1d10ef88f96513b8dc16c129c3ef1e3f700
kaitlynhova <kaitlynhova@gmail.com>
2014-06-10 12:46:07 -0500

    added nav annimations

---
+++ app/assets/javascripts/mobilenav.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var anchor = document.querySelectorAll('button');
+
+  [].forEach.call(anchor, function(anchor){
+    var open = false;
+    anchor.onclick = function(event){
+      event.preventDefault();
+      if(!open){
+        this.classList.add('close');
+        open = true;
+      }
+      else{
+        this.classList.remove('close');
+        open = false;
+      }
+    }
+  }); 
+});
