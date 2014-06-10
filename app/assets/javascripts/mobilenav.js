fb6d192151cccb446eed3d770ae3d4b91b3a6399
kaitlynhova <kaitlynhova@gmail.com>
2014-06-09 20:43:22 -0500

    started janky mobile menu

--- app/assets/javascripts/mobilenav.js
+++ app/assets/javascripts/mobilenav.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
   var anchor = document.querySelectorAll('button');
-
+  
   [].forEach.call(anchor, function(anchor){
     var open = false;
     anchor.onclick = function(event){
       event.preventDefault();
+      $('.overlay').toggleClass('overlay-scale');
+      $('.mobilenav_bar').toggleClass('no-bg');
       if(!open){
         this.classList.add('close');
         open = true;
