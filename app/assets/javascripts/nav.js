9a02e5c3a0a98db379a0f12e86b886522a9836ae
kaitlynhova <kaitlynhova@gmail.com>
2014-06-10 13:10:07 -0500

    mobile overlay style

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
-  $( "#music" ).click(function() {
-    alert("this");
-    $('#that_triangle').slideToggle('triangle_submenu');
-    $('#music_submenu').slideToggle('goaway');
-    if ($('#that_triangle').position().top == 68){
-      $('#that_triangle').css('display', 'none');
-    }else if ($('#that_triangle').position().top == 108){
-      $('#that_triangle').css('top', '68px');
-    }
-  });
-  $( "#media" ).click(function() {
-    $('#that_triangle').slideToggle('triangle_submenu');
-    $('#media_submenu').slideToggle('goaway');
-    if ($('#that_triangle').position().top == 68){
-      $('#that_triangle').css('top', '108px');
-    }else if ($('#that_triangle').position().top == 108){
-      $('#that_triangle').css('top', '68px');
-    }
+  
+  $('#trigger-overlay').click(function() {
+     $('.overlay').toggleClass('overlay_clicked');
+     
+     $('#trigger-overlay').css("z-index", 101);
+     
+     $('.mobilenav_bar').css("background","none");
   });
   
 });
