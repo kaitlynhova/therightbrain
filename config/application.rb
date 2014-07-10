875c7e091d8d6e37c4da7ebba9c499850a2e11a9
kaitlynhova <kaitlynhova@gmail.com>
2014-07-09 23:25:39 -0500

    favicon

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ end
 
 module TheRightBrain
   class Application < Rails::Application
-    
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
